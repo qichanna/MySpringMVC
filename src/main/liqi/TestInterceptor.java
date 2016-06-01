c9eca0f1f271c45491218398be62a84c3e398a30
qichanna <qichanna@126.com>
2016-06-01 13:24:40 +0800

    拦截器编码加入

--- src/main/liqi/TestInterceptor.java
+++ src/main/liqi/TestInterceptor.java
@@ -12,12 +12,24 @@ import javax.servlet.http.HttpServletResponse;
 public class TestInterceptor implements HandlerInterceptor{
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         System.out.println("pre handle");
+        // o表示的是被拦截的请求的目标对象
+        httpServletRequest.setCharacterEncoding("UTF-8");//编码问题
+
+        //登录验证问题
+        /*if(httpServletRequest.getSession().getAttribute("user") == null){
+            httpServletRequest.getRequestDispatcher("/login.jsp").forward(httpServletRequest,httpServletResponse);
+            return false;
+        }*/
         return true;
     }
 
     public void postHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, ModelAndView modelAndView) throws Exception {
         System.out.println("post handle");
+        // modelAndView.addObject("aa","bb"); 增加或修改页面中回传的值
+        // modelAndView.setViewName("/hello2.jsp"); 改变JSP视图
     }
+
+    //
 
     public void afterCompletion(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) throws Exception {
         System.out.println("after handle");
